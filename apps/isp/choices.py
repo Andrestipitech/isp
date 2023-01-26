fd8df6ceec694cbdc81657436a2a33184955a45f
AndresAlvaradow <andresalvarado34@gmail.com>
2023-01-25 21:10:30 -0500

    Modelos

---
+++ apps/isp/choices.py
@@ -0,0 +1,9 @@
+generos = (
+    ('Femenino', 'Femenino'), 
+    ('Masculino', 'Masculino')
+    )
+velocidad_planes =(
+    ('50','Basico'),
+    ('100', 'Premiun'),
+    ('200', 'Nitro')
+)
--- apps/isp/models.py
+++ apps/isp/models.py
@@ -1,42 +1,43 @@
 from django.db import models
-
+from .choices import generos, velocidad_planes
 # Create your models here.
 class clientes(models.Model):
-    dni = models.IntegerField(unique=True,max_length=10)
-    nombre = models.CharField()
-    apellido = models.CharField()
+    dni = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=70)
+    apellido = models.CharField(max_length=70)
     edad = models.IntegerField()
-    genero = models.CharField(choices=['masculino','femenino'])
-    domicilio = models.CharField()
+    genero = models.CharField(max_length=10,choices=generos, default = 'Femenino' )
+    domicilio = models.CharField(max_length=70)
     correo = models.EmailField()
 
     def __str__(self):
         return self.dni
 class planes(models.Model):
-    velocidad = models.CharField(choices=['50','100','200'])
+    id_plan=models.AutoField(primary_key=True)
+    velocidad = models.CharField(max_length=10,choices=velocidad_planes, default = '50')
     descripcion = models.TextField(max_length=100)
 
     def __str__(self):
         return self.velocidad
 
 class personal(models.Model):
-    genero = (('Femenino', 'Femenino'), ('Masculino', 'Masculino'))
-    dni = models.IntegerField(unique=True,max_length=10)
-    nombre = models.CharField()
-    apellido = models.CharField()
+    dni = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=70)
+    apellido = models.CharField(max_length=70)
     edad = models.IntegerField()
-    genero = models.CharField(choices=genero, default = 'Femenino')
-    domicilio = models.CharField()
+    genero = models.CharField(max_length=10,choices=generos, default = 'Femenino')
+    domicilio = models.CharField(max_length=70)
     correo = models.EmailField()
-    cargo = models.CharField()
+    cargo = models.CharField(max_length=70)
 
     def __str__(self):
         return self.cargo
 
 class vehiculos(models.Model):
-    tipo = models.CharField()
-    modelo = models.CharField()
-    placa = models.CharField()
+    placa = models.CharField(max_length=10, primary_key=True)
+    tipo = models.CharField(max_length=70)
+    modelo = models.CharField(max_length=75)
+    
     
     def __str__(self):
         return self.placa
