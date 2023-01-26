9f1db945b28eb89355be4b38ee7ff8f39419d165
AndresAlvaradow <andresalvarado34@gmail.com>
2023-01-25 20:39:30 -0500

    choices

--- apps/isp/models.py
+++ apps/isp/models.py
@@ -19,12 +19,13 @@ class planes(models.Model):
     def __str__(self):
         return self.velocidad
 
-class personal(models.Mo)
+class personal(models.Model):
+    genero = (('Femenino', 'Femenino'), ('Masculino', 'Masculino'))
     dni = models.IntegerField(unique=True,max_length=10)
     nombre = models.CharField()
     apellido = models.CharField()
     edad = models.IntegerField()
-    genero = models.CharField(choices=['masculino','femenino'])
+    genero = models.CharField(choices=genero, default = 'Femenino')
     domicilio = models.CharField()
     correo = models.EmailField()
     cargo = models.CharField()
