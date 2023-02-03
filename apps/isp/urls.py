b0e1e2c54e88142ca86324bb4097ac064985ca54
lein33 <sleandrojoel@hotmail.com>
2023-02-02 21:17:06 -0500

    form vehiculos y personal

--- apps/isp/urls.py
+++ apps/isp/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('isp/',views.prueba, name='index'),
     path('new_plan/',views.crear_plan, name='new_plan'),
     path('new_cliente/',views.crear_cliente, name='new_cliente'),
+    path('new_personal/',views.crear_personal, name='new_personal'),
+    path('new_vehiculo/',views.crear_vehiculo, name='new_vehiculo'),
+
 
 ]
